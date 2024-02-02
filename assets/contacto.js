e764eca26dacf7c68b90b62b3e63dbb9cc35e010
Jose Tejaxun <jtejaxun-2022229@kinal.edu.gt>
2024-02-02 09:14:01 -0600

    se finaliza apartado contactos

--- assets/contacto.js
+++ assets/contacto.js
@@ -1,7 +1,39 @@
 function mostrar(){
-    document.getElementById('detalles').style.display = '';
+    document.getElementById('detalles').style.display = 'flex';
 }
 
 function ocultar(){
     document.getElementById('detalles').style.display = 'none';
+}
+
+function mostrar1(){
+    document.getElementById('detalles1').style.display = 'flex';
+}
+
+function ocultar1(){
+    document.getElementById('detalles1').style.display = 'none';
+}
+
+function mostrar2(){
+    document.getElementById('detalles2').style.display = 'flex';
+}
+
+function ocultar2(){
+    document.getElementById('detalles2').style.display = 'none';
+}
+
+function mostrar3(){
+    document.getElementById('detalles3').style.display = 'flex';
+}
+
+function ocultar3(){
+    document.getElementById('detalles3').style.display = 'none';
+}
+
+function mostrar4(){
+    document.getElementById('detalles4').style.display = 'flex';
+}
+
+function ocultar4(){
+    document.getElementById('detalles4').style.display = 'none';
 }
